922a177ed2fb66dcb1cf4c8b859d2248648b7650
Madeleine Hill <mh11wi@outlook.com>
2022-12-08 16:26:55 -0500

    Increment version number

--- src/__tests__/Disk.test.js
+++ src/__tests__/Disk.test.js
@@ -2,14 +2,14 @@ import { render, screen } from '@testing-library/react';
 import Disk from '../lib/components/Disk';
 
 test('if text is undefined, Disk will render blank', () => {
-  render(<Disk ariaLabel="disk" text={undefined} radius={100} />);
+  render(<Disk ariaLabel="disk" text={undefined} radius={100} fontSize={16} />);
   const disk = screen.getByRole('button', { name: /disk/i });
   expect(disk.querySelectorAll('svg')).toHaveLength(1);
   expect(screen.getByTestId('column-0').textContent).toBe(' ');
 });
 
 test('if text is null, Disk will render blank', () => {
-  render(<Disk ariaLabel="disk" text={null} radius={100} />);
+  render(<Disk ariaLabel="disk" text={null} radius={100}  fontSize={16} />);
   const disk = screen.getByRole('button', { name: /disk/i });
   expect(disk.querySelectorAll('svg')).toHaveLength(1);
   expect(screen.getByTestId('column-0').textContent).toBe(' ');
@@ -17,7 +17,7 @@ test('if text is null, Disk will render blank', () => {
 
 test('if text is an array of strings, Disk will render the strings', () => {
   const text = ['a', 'b', 'c'];
-  render(<Disk ariaLabel="disk" text={text} radius={100} />);
+  render(<Disk ariaLabel="disk" text={text} radius={100} fontSize={16} />);
   const disk = screen.getByRole('button', { name: /disk/i });
   expect(disk.querySelectorAll('svg')).toHaveLength(3);
   
@@ -28,7 +28,7 @@ test('if text is an array of strings, Disk will render the strings', () => {
 
 test('if text is an array of numbers, Disk will render them as strings', () => {
   const text = [1, 2, 3];
-  render(<Disk ariaLabel="disk" text={text} radius={100} />);
+  render(<Disk ariaLabel="disk" text={text} radius={100} fontSize={16} />);
   const disk = screen.getByRole('button', { name: /disk/i });
   expect(disk.querySelectorAll('svg')).toHaveLength(3);
   
